fa8701faed1333e3b64bc7ace8616026a3348a69
Deathspike <>
2014-06-20 18:31:04 +0200

    Initial application commit.

--- README.md
+++ README.md
@@ -1,4 +1,62 @@
-promisifier.js
-==============
+# promisifier.js
 
 Converts callback-based APIs to ES6 promises with chainable convenience promises.
+
+## Installation
+
+	npm install promisifier.js
+
+## Compatibility
+
+Requires nodejs with at least version 0.11.x with ES6 features enabled (--harmony).
+
+## Recommendations
+
+A generator capable of pumping promises, such as [co](https://github.com/visionmedia/co), increases fun/productivity!
+
+## Examples
+
+### Basic
+
+	var promisifier = require('promisifier.js');
+	var fs = promisifier(require('fs'));
+	
+	fs.readFile('someKindOfMonster.txt', 'utf8').then(function (contents) {
+		console.log('contents: ' + contents);
+	}, function (error) {
+		console.log('error: ' + error);
+	});
+	
+### Errorless Chainable
+
+	var promisifier = require('promisifier.js');
+	var fs = promisifier(require('fs'));
+	
+	fs.exists('someKindOfMonster.txt').errorless().then(function (result) {
+		console.log('exists: ' + result[0]);
+	});
+	
+### Map Chainable
+
+	var promisifier = require('promisifier.js');
+	var request = promisifier(require('request'));
+
+	request('http://www.google.com/').map('response', 'body').then(function (result) {
+		console.log(result.response.statusCode);
+	});
+	
+### Yieldable
+
+	var co = require('co');
+	var promisifier = require('promisifier.js');
+	var request = promisifier(require('request'));
+
+	co(function *() {
+		var a = yield request('http://google.com').map('response', 'body');
+		var b = yield request('http://yahoo.com').map('response', 'body');
+		var c = yield request('http://cloudup.com').map('response', 'body');
+		var all = yield [a, b, c];
+		console.log(all[0].response.statusCode);
+		console.log(all[1].response.statusCode);
+		console.log(all[2].response.statusCode);
+	})();
---
+++ index.js
@@ -0,0 +1,128 @@
+// Enable restricted mode.
+'use strict';
+// Initialize the errorless function.
+let errorless;
+// Initialize the make function.
+let make;
+// Initialize the map function.
+let map;
+
+// ==================================================
+// Export the function.
+// --------------------------------------------------
+module.exports = function (object) {
+	// Initialize the promise factory.
+	let promiseFactory = typeof object !== 'function' ? {} : function () {
+		// Initialize the promise.
+		let promise = make(this, Array.prototype.slice.call(arguments), object);
+		// Add the errorless function.
+		promise.errorless = errorless(this, promise);
+		// Add the map function.
+		promise.map = map(this, promise);
+		// Return the promise.
+		return promise;
+	};
+	// Iterate through each key.
+	for (let key in object) {
+		// Check if the key has not been inherited.
+		if (object.hasOwnProperty(key) && !/_$/.test(key)) {
+			// Add the encapsulation.
+			promiseFactory[key] = module.exports(object[key]);
+		}
+	}
+	// Return the promise factory.
+	return promiseFactory;
+};
+
+// ==================================================
+// Create a promise with each chainable promise.
+// --------------------------------------------------
+let chainable = function (callback) {
+	// Initialize the promise.
+	let promise = new Promise(callback);
+	// Add the errorless function.
+	promise.errorless = errorless(this, promise);
+	// Add the map function.
+	promise.map = map(this, promise);
+	// Return the promise.
+	return promise;
+};
+
+// ==================================================
+// Create a promise which passes an error as success.
+// --------------------------------------------------
+errorless = function (context, promise) {
+	// Return the encapsulation function.
+	return function () {
+		// Set the reject enforcement state.
+		promise._enforceReject = true;
+		// Return the promise.
+		return chainable(function (resolve) {
+			// Wait for the bound promise.
+			promise.then(resolve, function () {
+				// Resolve the promise.
+				resolve.apply(context, Array.prototype.slice.call(arguments));
+			});
+		});
+	};
+};
+
+// ==================================================
+// Create a promise encapsulation.
+// --------------------------------------------------
+make = function (context, localArgs, callback) {
+	// Initialize the promise.
+	let promise = chainable(function (resolve, reject) {
+		// Invoke the callback function.
+		let result = callback.apply(context, localArgs.concat(function (error) {
+			// Check if an error has occurred.
+			if (error || promise._enforceReject) {
+				// Reject the promise.
+				reject.call(this, Array.prototype.slice.call(arguments));
+				// Stop the function.
+				return;
+			}
+			// Resolve the promise.
+			resolve.call(this, Array.prototype.slice.call(arguments, 1));
+		}));
+		// Check if the callback returned a promise.
+		if (result && typeof result.then === 'function') {
+			// Wait for the resulting promise.
+			result.then(resolve, reject);
+		}
+	});
+	// Return the promise.
+	return promise;
+};
+
+// ==================================================
+// Create a promise which maps an array to an object.
+// --------------------------------------------------
+map = function (context, promise) {
+	// Return the encapsulation function.
+	return function (map) {
+		// Initialize the map.
+		map = Array.isArray(map) ? map : Array.prototype.slice.call(arguments);
+		// Return the promise.
+		return chainable(function (resolve, reject) {
+			// Wait for the promise.
+			promise.then(function (value) {
+				// Check if the value is valid.
+				if (Array.isArray(value)) {
+					// Initialize the result.
+					let result = {};
+					// Iterate through each value.
+					for (let i = 0; i < map.length; i += 1) {
+						// Map the value to the result.
+						result[map[i]] = i < value.length ? value[i] : undefined;
+					}
+					// Resolve the promise with the result.
+					resolve(result);
+				} else {
+					// Resolve the promise.
+					resolve(value);
+				}				
+			}, reject);
+		});
+	};
+};
